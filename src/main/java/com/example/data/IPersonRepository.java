fde4ef221802bb63627e0d477ca258a364e95d6a
StewartRowney <110835640+StewartRowney@users.noreply.github.com>
2023-11-24 12:01:00 +0000

    Re-versioned all files

--- src/main/java/com/example/data/IPersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.data;
-
-import com.example.entities.Person;
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface IPersonRepository extends ListCrudRepository<Person, Long> {
-
-}
